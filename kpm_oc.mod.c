840b9d1cbaeb6c01bf0451d20a4a9c1e813025ef
zerofrip <zerofripside@gmail.com>
2026-03-30 19:30:31 +0900

    build: refresh kpm_oc.mod.c after v6.8 relift updates

--- kpm_oc.mod.c
+++ kpm_oc.mod.c
@@ -22,7 +22,7 @@ __section(".gnu.linkonce.this_module") = {
 	.arch = MODULE_ARCH_INIT,
 };
 
-MODULE_INFO(scmversion, "g03d1dd8f6d40-dirty");
+MODULE_INFO(scmversion, "gb01e56b4244f");
 
 #ifdef CONFIG_RETPOLINE
 MODULE_INFO(retpoline, "Y");
